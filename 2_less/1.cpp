3402ecfa1fd6b5555df55efb6b054ec32333fd9a
julproh <cherry5005@mail.ru>
2021-02-13 17:33:02 +0300

    new lesson

---
+++ 2_less/1.cpp
@@ -0,0 +1,22 @@
+/*
+1 - untracked
+2 - unmodifed
+3 - modified
+4 - staged (add)
+
+done - клонирование репозитория
+add - добавить файл к commity
+remove - убрать файл из commity
+pull - стянуть commity  из irigin и применить
+commit - утвердить изменения в репозитории
+fetch - -//- без приминения
+push - закинуть commit  с локального в origin
+branch - работа с ветками
+checkout - переключение между вектами
+merge - c   и   н   я
+rebase -  л   я   и 
+hist / log
+status
+tag - добавляет меточку
+*/
+
