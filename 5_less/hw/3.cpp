fbf007ec686c2b15b16016c995f1341692d2e499
julproh <cherry5005@mail.ru>
2021-03-18 20:05:09 +0300

    new changes

--- 5_less/hw/3.cpp
+++ 5_less/hw/3.cpp
@@ -11,22 +11,25 @@ int main()
                     "alex-27@yandex.com",
                     "alex.27@yandex.com",
                     "alex111@devcolibri.com",
-                    "alex.100@devcolibri.com.ua",
-                    "alex@1.com",
+                    "alex100@devcolibri.com.ua",
+                    "alex@buf1.com",
                     "alex@gmail.com.com",
                     "alex+27@gmail.com",
-                    "alex-27@yandex-test.com"};
+                    "alex-27@yandex-test.com",
+                    "re34@q.w.e"};
 
 	std::regex regular("([\\w-]+)"
                         "(@)"
                         "([\\w-]+)"
-                        "(\.)"
-                        "([A-Za-z]{2,4})");
+                        "(\\.)"
+                        "([A-Za-z]{2,6})"
+                        "((\\.[A-Za-z]{2,6}){0,1})");
 
     for (auto i : v) {
         cmatch result;
-        if (std::regex_match(i.c_str(), result, regular)) 
-            cout << result[0] << " - " << result[3] << result[4] << result[5] << endl;
+        if (std::regex_match(i.c_str(), result, regular)) {
+            cout << result[0] << " - " << result[3] << result[4] << result[5] << result[6] << endl; 
+        }
         else 
             cout << i << " - неверная запись" << endl; 
     }
